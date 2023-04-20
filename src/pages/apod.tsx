3071166d6749fc3e40c3eecdbe6778367aeedf89
Jesus Flores <jesus.flores1008@gmail.com>
2023-04-20 16:13:58 -0700

    Loading spinner for nasa

--- src/pages/apod.tsx
+++ src/pages/apod.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
+
+import LoadingComponent from "./loading";
+
 type apod = {
     copyright: string,
     date: string,
@@ -9,50 +12,54 @@ type apod = {
     service_version: number,
     title: string,
     url: string
-  }
+}
 
 
 export default function Apod() {
 
-
+  const [isLoading, setIsLoading] = useState(true);
   const [imgObj, setImgObj] = useState<apod>();
 
-    useEffect(() => {
-        async function fetchData() {
-        // Send the form data to our API and get a response.
-        const response = await fetch('/api/apod', {
-        // The method is POST because we are sending data.
-        method: 'GET',
-        })
+  useEffect(() => {
+      async function fetchData() {
+      // Send the form data to our API and get a response.
+      const response = await fetch('/api/apod', {
+      // The method is POST because we are sending data.
+      method: 'GET',
+      })
 
-        // Get the response data from server as JSON.
-        const result = await response.json();
-        console.log(result)
-        setImgObj(result.data as apod);
-            
-        }
-        fetchData();
-    },[])
+      // Get the response data from server as JSON.
+      const result = await response.json();
+      console.log(result)
+      setImgObj(result.data as apod);
+      setIsLoading(false)
+      }
+      fetchData();
+  },[])
 
 
   return (
+    <>
 
+      {isLoading && <LoadingComponent message="Fetching Picture..." /> }   
 
-    <div className="flex flex-col items-center bg-white dark:bg-gray-800 mt-4 text-gray-800 dark:text-white">
-        {imgObj !== undefined && (
-            <div className="flex flex-col items-center p-4 rounded-lg shadow-lg">
-            <p className="text-gray-800 dark:text-white">{imgObj["date"]}</p>
-            <Image
-                className="rounded-md"
-                src={imgObj["url"]}
-                alt="new"
-                width={512}
-                height={512}
-            />
-            <p className="text-gray-800 dark:text-white">{imgObj["explanation"]}</p>
-            </div>
-        )}
-    </div>
+      <div className="flex flex-col items-center bg-white dark:bg-gray-800 mt-4 text-gray-800 dark:text-white">
+          {imgObj !== undefined && (
+              <div className="flex flex-col items-center p-4 rounded-lg shadow-lg">
+                <p className="text-gray-800 dark:text-white">{imgObj["title"]}</p>
+                <p className="text-gray-800 dark:text-white">{imgObj["date"]}</p>
+                <Image
+                    className="rounded-md"
+                    src={imgObj["url"]}
+                    alt="new"
+                    width={512}
+                    height={512}
+                />
+                <p className="text-gray-800 dark:text-white">{imgObj["explanation"]}</p>
+              </div>
+          )}
 
+      </div>
+    </>
   );
 }
--- src/pages/navBar.tsx
+++ src/pages/navBar.tsx
@@ -1,43 +1,38 @@
 import Link from 'next/link';
 
 function NavBar() {
-return (
-  <div className=' bg-white dark:bg-gray-800 mt-4 text-gray-800 dark:text-white'> 
-    <div className="p-4 rounded-lg shadow-lg">
-          
-          <Link href="/">
-            <p className="text-gray-800 dark:text-white font-bold text-lg">
-              datai
-            </p>
-          </Link>
+  return (
+    <div className=' bg-white dark:bg-gray-800 mt-4 text-gray-800 dark:text-white'> 
+      <div className="p-4 rounded-lg shadow-lg">
+            
+            <Link href="/">
+              <p className="text-gray-800 dark:text-white font-bold text-lg">
+                datai
+              </p>
+            </Link>
 
-          <Link href="/exercises">
-            <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover: hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
-              Gym Exercises
-            </p>
-          </Link>
+            <Link href="/exercises">
+              <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover: hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
+                Gym Exercises
+              </p>
+            </Link>
 
-          <Link href="/name">
-            <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover: hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
-              Name classifier
-            </p>
-          </Link>
+            <Link href="/name">
+              <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover: hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
+                Name classifier
+              </p>
+            </Link>
 
-          <Link href="/apod">
-            <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover: hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
-              Astronomy Pic of the Day
-            </p>
-          </Link>
+            <Link href="/apod">
+              <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover: hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
+                Astronomy Pic of the Day
+              </p>
+            </Link>
 
-      </div>
-  </div>
+        </div>
+    </div>
 
   );
 }
 
 export default NavBar;
-
-
-
-
-
