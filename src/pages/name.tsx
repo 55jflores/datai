1beeb344abed575c7b3149b58c846dee4fefa255
Jesus Flores <jesus.flores1008@gmail.com>
2023-04-24 20:19:28 -0700

    moved script out of head

--- src/pages/name.tsx
+++ src/pages/name.tsx
@@ -1,12 +1,8 @@
-import Head from "next/head";
 import Script from "next/script";
 export default function Name() {
   return (
     <div className='flex flex-col items-center text-gray-800 dark:text-white'>
-      <Head>
-        <Script src="https://gradio.s3-us-west-2.amazonaws.com/3.27.0/gradio.js" />
-      </Head>
-
+      <Script src="https://gradio.s3-us-west-2.amazonaws.com/3.27.0/gradio.js" />
       <gradio-app src="https://jflo-classifylastname.hf.space"></gradio-app>
     </div>
   )
