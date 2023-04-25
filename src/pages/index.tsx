16ca923029fff0f33e0c34127034eaf15384c0f4
Jesus Flores <jesus.flores1008@gmail.com>
2023-04-24 20:51:59 -0700

    added to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
 
   return (
@@ -6,7 +8,7 @@ export default function Home() {
       
       <div className='flex flex-col items-center text-gray-800 dark:text-white'>
         <h1>Welcome!</h1>
-        <h2>Named datai because I give you data and sometimes I use AI to do so</h2>
+        <p>Named datai because I give you data and sometimes I use AI to do so</p>
         <p>Click on a tab above to interact with projects I have created!</p>
         <p>Website still a work in progress... obviously lol</p>
 
