c3dd10ae29deed83076a58c2bac6b4bee4ced68e
Jesus Flores <jesus.flores1008@gmail.com>
2023-04-24 20:36:15 -0700

    moved script out of head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       
       <div className='flex flex-col items-center text-gray-800 dark:text-white'>
         <h1>Welcome!</h1>
+        <h2>Named datai because I give you data and sometimes I use AI to do so</h2>
         <p>Click on a tab above to interact with projects I have created!</p>
         <p>Website still a work in progress... obviously lol</p>
 
