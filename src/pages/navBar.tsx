192623267f81df40247c24c0daf5d9d982720933
Jesus Flores <jesus.flores1008@gmail.com>
2023-04-25 19:36:28 -0700

    Added menu tab for navigation

--- src/pages/navBar.tsx
+++ src/pages/navBar.tsx
@@ -1,38 +1,110 @@
+import { useState } from 'react';
 import Link from 'next/link';
 
-function NavBar() {
+const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className=' bg-white dark:bg-gray-800 mt-4 text-gray-800 dark:text-white'> 
-      <div className="p-4 rounded-lg shadow-lg">
-            
+    <nav className="bg-gray-100 dark:bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              onClick={() => setMenuOpen(!menuOpen)}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-800 dark:text-white hover:text-gray-500 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {menuOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <Link href="/">
               <p className="text-gray-800 dark:text-white font-bold text-lg">
                 datai
               </p>
             </Link>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <Link href="/exercises">
+                  <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
+                    Gym Exercises
+                  </p>
+                </Link>
+                <Link href="/name">
+                  <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
+                    Last Name Classifier
+                  </p>
+                </Link>
+                <Link href="/apod">
+                  <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
+                    Astronomy Pic of the Day
+                  </p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-            <Link href="/exercises">
-              <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover: hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
+     {/* Mobile menu, show/hide based on menu state */}
+     {menuOpen && (
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            <Link href="/exercises" onClick={() => setMenuOpen(!menuOpen)}>
+              <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover:text-gray-300 dark:hover:text-gray-800 block px-3 py-2 rounded-md text-base font-medium">
                 Gym Exercises
               </p>
             </Link>
-
-            <Link href="/name">
-              <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover: hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
+            <Link href="/name" onClick={() => setMenuOpen(!menuOpen)}>
+              <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover:text-gray-300 dark:hover:text-gray-800 block px-3 py-2 rounded-md text-base font-medium">
                 Last Name Classifier
               </p>
             </Link>
-
-            <Link href="/apod">
-              <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover: hover:text-gray-300 dark:hover:text-gray-800 px-3 py-2 rounded-md text-sm font-medium">
+            <Link href="/apod" onClick={() => setMenuOpen(!menuOpen)}>
+              <p className="text-gray-800 dark:text-white hover:bg-gray-500 hover:text-gray-300 dark:hover:text-gray-800 block px-3 py-2 rounded-md text-base font-medium">
                 Astronomy Pic of the Day
               </p>
             </Link>
-
+          </div>
         </div>
-    </div>
-
+      )}
+    </nav>
   );
-}
+};
 
 export default NavBar;
